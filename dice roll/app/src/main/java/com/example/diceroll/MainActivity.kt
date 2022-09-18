b88947e7b9013c58c9532d031516ab0beb3b17e0
spystarspo <109082271+spystarspo@users.noreply.github.com>
2022-09-18 22:54:50 +0530

    dice roll changes

--- dice roll/app/src/main/java/com/example/diceroll/MainActivity.kt
+++ dice roll/app/src/main/java/com/example/diceroll/MainActivity.kt
@@ -17,7 +17,7 @@ package com.example.diceroll
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 
@@ -44,14 +44,32 @@ class MainActivity : AppCompatActivity() {
     /**
      * Roll the dice and update the screen with the result.
      */
+    /**
+     * Roll the dice and update the screen with the result.
+     */
     private fun rollDice() {
-        // Create new Dice object with 6 sides and roll it
+        // Create new Dice object with 6 sides and roll the dice
         val dice = Dice(6)
         val diceRoll = dice.roll()
 
-        // Update the screen with the dice roll
-        val resultTextView: TextView = findViewById(R.id.textView2)
-        resultTextView.text = diceRoll.toString()
+        // Find the ImageView in the layout
+        val diceImage: ImageView = findViewById(R.id.imageView1)
+
+        // Determine which drawable resource ID to use based on the dice roll
+        val drawableResource = when (diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        // Update the ImageView with the correct drawable resource ID
+        diceImage.setImageResource(drawableResource)
+
+        // Update the content description
+        diceImage.contentDescription = diceRoll.toString()
     }
 }
 
